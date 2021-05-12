3e51acbfe772f3a0e147042883ba5fbc523a94af
Mohamed <mohamedrocket72@gmail.com>
2021-05-12 01:33:15 -0700

    comit

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,Image,Button,Animated } from 'react-native';
+import GLOBAL from "./Global";
+var money = 0;
+//export default function App() {
+export default class App extends Component{
+  state = {
+    bucket:0
+  };
+  render(){
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "GillSans-UltraBold", fontSize:50, bottom:'10%',alignItems:'center', justifyContent:"center",textShadowColor: 'rgba(255, 0, 0, 1)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 15}}>Ball Stats</Text>
+
+        <Image style={{width:200, height:200}}source={require("./assets/ball.png")} />
+
       <StatusBar style="auto" />
+
+      <Button
+  
+                // Some properties given to Button
+                title="Score"
+                //onPress={() => console.log(GLOBAL.mo+=1)}
+                onPress={() => this.setState({bucket: this.state.bucket+1})}
+      />
+      <Text>{this.state.bucket}</Text>
+    
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Global.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mo:0
+  };
