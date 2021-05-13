ca41f573f73d2e0412f71acec5c0b39db10d8f8f
Mohamed <mohamedrocket72@gmail.com>
2021-05-13 01:02:51 -0700

    commit

--- Global.js
+++ Global.js
@@ -1,3 +1,13 @@
 module.exports = {
-    mo:0
-  };
+  firstname: null,
+  lastname: null,
+  Full_Name: null,
+  Google: 0,
+  Facebook: 0,
+  profilepic: null,
+  Usernew: null,
+
+  CreateAccount: false,
+
+  prac: [],
+};
